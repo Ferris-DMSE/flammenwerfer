5da1e6b651d928b68b8abafd44cb50ef9553cef1
Do-Matrix <domatrix6@gmail.com>
2016-12-08 02:16:04 -0500

    updated process, need to update file to not crash on run. File path is explicit currently to location per machine for debug purposes

--- Flammenwerfer UWP/Flammenwerfer UWP/MainPage.xaml.cs
+++ Flammenwerfer UWP/Flammenwerfer UWP/MainPage.xaml.cs
@@ -70,18 +70,19 @@ namespace Flammenwerfer_UWP
         private void lnameRadioButton_Checked(object sender, RoutedEventArgs e)
         {
             textBlock.Text = "Enter students last name";
+            QueryType = "lname";
         }
 
         private void sidRadioButton_Checked(object sender, RoutedEventArgs e)
         {
             textBlock.Text = "Enter students I.D. #";
-
+            QueryType = "sid";
         }
 
         private void fnameRadioButton_Checked(object sender, RoutedEventArgs e)
         {
             textBlock.Text = "Enter students first name";
-
+            QueryType = "fname";
         }
     }
 }
--- Flammenwerfer UWP/Flammenwerfer UWP/Query_Search.cs
+++ Flammenwerfer UWP/Flammenwerfer UWP/Query_Search.cs
@@ -19,7 +19,7 @@ namespace Flammenwerfer
         private List<string> studentsFoundInQuery;
         public List<string> StudentsFoundInQuery { get { return studentsFoundInQuery; } }
 
-        public void Search(string sSearchParamater, string type)
+        public async void Search(string sSearchParamater, string type)
         {
             List<string> lFoundStudent = new List<string>();//this array will be used to send the found information to the next class
             lFoundStudent.Add(""); //used to 
@@ -29,62 +29,69 @@ namespace Flammenwerfer
             bool bStudentFound = false;//will be set to true if student is found
             int iCourseCounter = 0;
             string SearchNode = "";
-            XDocument xmlArchive = new XDocument();
-            var spath = @"C:/Users/domat/Source/Repos/flammenwerfer/Flammenwerfer UWP/ Flammenwerfer UWP/UserData.xml";
-            XNamespace xpath = spath;
-            //loads the precreated xml doc,takes the path string found in the XML_Creator class
-            var Student = xmlArchive.Descendants(xpath + "Student").ToList();
-            var Courses = xmlArchive.Descendants(xpath + "Course").ToList();
-
-            switch (type)//here starts a check for each type of search(ID, First Name and Last Name)
+            var spath = "D:/UserData.xml";
+            var file = await StorageFile.GetFileFromPathAsync(spath);
+            using (var stream = await file.OpenStreamForReadAsync())
             {
-                case "sid":
-                    SearchNode = "SID";
-                    break;
+                XDocument xmlArchive = XDocument.Load(spath);
+                var StudentParamList = xmlArchive.Descendants("Students");
+                var studentParamList = StudentParamList.ToArray();
+                int studentcount = xmlArchive.Descendants("Student").Count();
+                var CourseParamList = xmlArchive.Descendants("Courses");
+                var courseParamList = CourseParamList.ToArray();
+                int corsecount = xmlArchive.Descendants("Course").Count();
 
-                case "fname":
-                    SearchNode = "FName";
-                    break;
-
-                case "lname":
-                    SearchNode = "LName";
-                    break;
-
-                default: //If no type was properly selected
-                    //Console.WriteLine("search paramaters incorrect please see system analyst");
-                    //Console.ReadKey();
-                    break;
-            }
-
-            foreach (var Node in Student)
-            {
-                sArchiveSearch = Node.Attribute(SearchNode).Value.ToString();
-                if (sArchiveSearch == sSearchParamater)//if the searched name equal and achieved name then sFoundStudent is filled with the the information from the archieve
+                switch (type)//here starts a check for each type of search(ID, First Name and Last Name)
                 {
-                    bStudentFound = true;
-                    sSearchedUID = Node.Attribute("SID").Value.ToString();
-                    lFoundStudent.Add(sSearchedUID);
-                    lFoundStudent.Add(Node.Attribute("FName").Value.ToString());
-                    lFoundStudent.Add(Node.Attribute("LName").Value.ToString());
-                    foreach (var xNode in Courses)
+                    case "sid":
+                        SearchNode = "SID";
+                        break;
+
+                    case "fname":
+                        SearchNode = "FName";
+                        break;
+
+                    case "lname":
+                        SearchNode = "LName";
+                        break;
+
+                    default:
+                        SearchNode = "FName";
+                        break;
+                }
+
+                for (int x = 0; x < studentcount; x++)
+                {
+                    sArchiveSearch = studentParamList[x].Attribute(SearchNode).Value.ToString();
+                    if (sArchiveSearch == sSearchParamater)//if the searched name equal and achieved name then sFoundStudent is filled with the the information from the archieve
                     {
-                        sArchiveUID = xNode.Attribute("UID").Value.ToString();
-                        if (sArchiveUID == sSearchedUID)
+                        bStudentFound = true;
+                        sSearchedUID = studentParamList[x].Attribute("SID").Value.ToString();
+                        lFoundStudent.Add(sSearchedUID);
+                        lFoundStudent.Add(studentParamList[x].Attribute("FName").Value.ToString());
+                        lFoundStudent.Add(studentParamList[x].Attribute("LName").Value.ToString());
+                        for (int i = 0; i < corsecount; i++)
                         {
-                            iCourseCounter++;
-                            lFoundStudent.Add(xNode.Attribute("CourseID").Value.ToString());
-                            lFoundStudent.Add(xNode.Attribute("CourseNumber").Value.ToString());
-                            lFoundStudent.Add(xNode.Attribute("CourseName").Value.ToString());
-                            lFoundStudent.Add(xNode.Attribute("Credits").Value.ToString());
-                            lFoundStudent.Add(xNode.Attribute("Year").Value.ToString());
-                            lFoundStudent.Add(xNode.Attribute("Semester").Value.ToString());
-                            lFoundStudent.Add(xNode.Attribute("CourseType").Value.ToString());
-                            lFoundStudent.Add(xNode.Attribute("CourseGrade").Value.ToString());
+                            sArchiveUID = courseParamList[i].Attribute("UID").Value.ToString();
+                            if (sArchiveUID == sSearchedUID)
+                            {
+                                iCourseCounter++;
+                                lFoundStudent.Add(courseParamList[i].Attribute("CourseID").Value.ToString());
+                                lFoundStudent.Add(courseParamList[i].Attribute("CourseNumber").Value.ToString());
+                                lFoundStudent.Add(courseParamList[i].Attribute("CourseName").Value.ToString());
+                                lFoundStudent.Add(courseParamList[i].Attribute("Credits").Value.ToString());
+                                lFoundStudent.Add(courseParamList[i].Attribute("Year").Value.ToString());
+                                lFoundStudent.Add(courseParamList[i].Attribute("Semester").Value.ToString());
+                                lFoundStudent.Add(courseParamList[i].Attribute("CourseType").Value.ToString());
+                                lFoundStudent.Add(courseParamList[i].Attribute("CourseGrade").Value.ToString());
+                            }
+                            lFoundStudent[0] = Convert.ToString(iCourseCounter);
                         }
-                        lFoundStudent[0] = Convert.ToString(iCourseCounter);
                     }
                 }
             }
+            //loads the precreated xml doc,takes the path string found in the XML_Creator class
+
             if (bStudentFound == true)
             {
                 studentsFoundInQuery = lFoundStudent;
