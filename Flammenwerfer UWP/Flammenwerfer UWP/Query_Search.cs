fc2973e6f58b753529e8790deec4655fb937bb70
Do-Matrix <domatrix6@gmail.com>
2016-12-13 05:08:59 -0500

    fixed test logic to fit current code. need to fix null data pull errors

--- Flammenwerfer UWP/Flammenwerfer UWP/Query_Search.cs
+++ Flammenwerfer UWP/Flammenwerfer UWP/Query_Search.cs
@@ -23,6 +23,9 @@ namespace Flammenwerfer
 
             List<string> lFoundStudent = new List<string>();//this list will be used to send the found information to the next class
             lFoundStudent.Add(""); //used to be a placeholder for number of courses a student has on record
+            string sSearchedStudent = "";
+            int index = 0;
+            int caseOffset = 0;
             string sSearchedUID = ""; //store variable to searching UID variable
             bool bStudentFound = false;//will be set to true if student is found
             int iCourseCounter = 0;
@@ -33,64 +36,64 @@ namespace Flammenwerfer
             xmlArchive.LoadXml(xmlString);//loads the precreated xml doc,takes the path string found in the XML_Creator class
             XmlNodeList XNList = xmlArchive.SelectNodes("/Students/Student");
             XmlNodeList XNListCourses = xmlArchive.SelectNodes("/Students/Student/Courses/Course");
-            var typeNode = "/Students/Student/" + type;
-            foreach (var Node in XNList)
+            while (index < XNList.Count)
             {
-                if (Node.NodeName == type && Node.InnerText == sSearchParamater)
+                switch (type)
                 {
-                    bStudentFound = true;
-                    switch(Node.NodeName)
-                    {
-                        case "SID":
-                            lFoundStudent.Add(Node.InnerText);
-                            sSearchedUID = Node.InnerText;
-                            break;
-                        case "FName":
-                            lFoundStudent.Add(Node.InnerText);
-                            break;
-                        case "LName":
-                            lFoundStudent.Add(Node.InnerText);
-                            break;
-                    }
+                    case "FName":
+                        sSearchedStudent = XNList[index].InnerText;
+                        caseOffset = 1;
+                        break;
+                    case "LName":
+                        sSearchedStudent = XNList[index].InnerText;
+                        caseOffset = 2;
+                        break;
+                    case "SID":
+                        sSearchedStudent = XNList[index].InnerText;
+                        caseOffset = 0;
+                        break;
                 }
+                if (sSearchedStudent == sSearchParamater)
+                    break;
+                index++;
             }
-            foreach (var xNode in XNListCourses)
+            lFoundStudent.Add(XNList[(index - caseOffset)].InnerText);
+            sSearchedUID = XNList[(index - caseOffset)].InnerText;
+            lFoundStudent.Add(XNList[(index - caseOffset + 1)].InnerText);
+            lFoundStudent.Add(XNList[(index - caseOffset + 2)].InnerText);
+            index = 0;
+            caseOffset = 0;
+            while (index < XNListCourses.Count)
             {
-                if (xNode.NodeName == "UID" && xNode.InnerText == sSearchedUID)
+                if (XNListCourses[index].NodeName == "UID")
                 {
-                    iCourseCounter++;
-                    switch (xNode.NodeName)
+                    if (XNListCourses[index].InnerText == sSearchedUID)
                     {
-                        case "CourseID":
-                            lFoundStudent.Add(xNode.InnerText);
-                            break;
-                        case "CourseNumber":
-                            lFoundStudent.Add(xNode.InnerText);
-                            break;
-                        case "CourseName":
-                            lFoundStudent.Add(xNode.InnerText);
-                            break;
-                        case "Credits":
-                            lFoundStudent.Add(xNode.InnerText);
-                            break;
-                        case "Year":
-                            lFoundStudent.Add(xNode.InnerText);
-                            break;
-                        case "Semester":
-                            lFoundStudent.Add(xNode.InnerText);
-                            break;
-                        case "CourseType":
-                            lFoundStudent.Add(xNode.InnerText);
-                            break;
-                        case "CourseGrade":
-                            lFoundStudent.Add(xNode.InnerText);
-                            break;
-                    }
-                }
-                lFoundStudent[0] = iCourseCounter.ToString();
-            }
-            //loads the precreated xml doc,takes the path string found in the XML_Creator class
+                        iCourseCounter++;
+                        var coursedataindex = index + 1;
+                        lFoundStudent.Add(XNListCourses[(coursedataindex)].InnerText);
+                        coursedataindex++;
+                        lFoundStudent.Add(XNListCourses[(coursedataindex)].InnerText);
+                        coursedataindex++;
+                        lFoundStudent.Add(XNListCourses[(coursedataindex)].InnerText);
+                        coursedataindex++;
+                        lFoundStudent.Add(XNListCourses[(coursedataindex)].InnerText);
+                        coursedataindex++;
+                        lFoundStudent.Add(XNListCourses[(coursedataindex)].InnerText);
+                        coursedataindex++;
+                        lFoundStudent.Add(XNListCourses[(coursedataindex)].InnerText);
+                        coursedataindex++;
+                        lFoundStudent.Add(XNListCourses[(coursedataindex)].InnerText);
+                        coursedataindex++;
+                        lFoundStudent.Add(XNListCourses[(coursedataindex)].InnerText);
+                        coursedataindex++;
 
+                    }
+                }
+                index++;
+            }
+            lFoundStudent[0] = iCourseCounter.ToString();
+            //loads the precreated xml doc,takes the path string found in the XML_Creator class
             if (bStudentFound == true)
             {
                 studentsFoundInQuery = lFoundStudent;
